1085be5f1548d9d6e853b42eb654112a92a06177
Cesar-eav <cesar.eav@hotmail.es>
2024-07-28 11:15:10 -0400

    Menu completo sin links

--- resources/js/Components/Sidebar/SidebarContent.vue
+++ resources/js/Components/Sidebar/SidebarContent.vue
@@ -21,63 +21,198 @@
 
 
             </SidebarCollapsible>
+            
             <SidebarCollapsible title="Chancao Primario" :href="route('mapa2')" :active="route().current('mapa2')">
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
             </SidebarCollapsible>
 
             <SidebarCollapsible title="Chancao Secundario" :href="route('mapa2')" :active="route().current('mapa2')">
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
             </SidebarCollapsible>
 
             <SidebarCollapsible title="Chancao Terciario" :href="route('mapa2')" :active="route().current('mapa2')">
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
             </SidebarCollapsible>
 
-            <SidebarCollapsible title="Torre de Transferencia" :href="route('mapa2')"
-                :active="route().current('mapa2')">
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
+
+            <SidebarCollapsible title="Torre de Transferencia" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
             </SidebarCollapsible>
 
+ 
             <SidebarCollapsible title="Aglomerado" :href="route('mapa2')" :active="route().current('mapa2')">
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
-                <SidebarCollapsibleItem title="Item" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
             </SidebarCollapsible>
 
+
+            <SidebarCollapsible title="Pilas de Lixiviación" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+            <SidebarCollapsible title="Pilas Rom Lix" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+            <SidebarCollapsible title="Ripios" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+
+            <SidebarCollapsible title="Planta Piloto" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+
+            <SidebarCollapsible title="Muestra Geológica" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+            
+        <SidebarCollapsible title="Geología" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+
+            <SidebarCollapsible title="Lab. Químico" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+            <SidebarCollapsible title="Campamento" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+            <SidebarCollapsible title="Nave EW" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+
+
+            <SidebarCollapsible title="Nave SX" :href="route('mapa2')" :active="route().current('mapa2')">
+                <SidebarCollapsibleItem title="Energía Mecánica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Hidraulica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Cinética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Neumática" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Térmica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Eólica" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Magnética" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Solar" :href="'#'"> </SidebarCollapsibleItem>
+                <SidebarCollapsibleItem title="Energía Geotérmica" :href="'#'"> </SidebarCollapsibleItem>
+            </SidebarCollapsible>
+            
         </SidebarCollapsible>
-        <!-- 
-        <SidebarCollapsible title="Chancao Primario" :href="route('mapa1')" :active="route().current('mapa1')">
-            <SidebarCollapsibleItem title="Aglomerado" :href="'#'"> </SidebarCollapsibleItem>
-            <SidebarCollapsibleItem title="Aglomerado" :href="'#'"> </SidebarCollapsibleItem>
 
-        </SidebarCollapsible> -->
-
-
-        <!-- 
-        <SidebarCollapsibleItem title="Chancao Primario"> </SidebarCollapsibleItem>
-        <SidebarCollapsibleItem title="Proceso Mina">
-        </SidebarCollapsibleItem>
-
-        <SidebarCollapsibleItem title="Chancao Secundario" :href="'#'"> </SidebarCollapsibleItem>
-
-        <SidebarCollapsibleItem title="Chancao Terciario" :href="'#'"> </SidebarCollapsibleItem>
-
-        <SidebarCollapsibleItem title="Torre de Transferencia" :href="'#'"> </SidebarCollapsibleItem>
-
-        <SidebarCollapsibleItem title="Aglomerado" :href="'#'"> </SidebarCollapsibleItem> -->
-
-
-
-
-        <!-- <SidebarCollapsible title="Siniestros">
-            <li>-Crear Siniestros</li>
-            <li>-Listado de Siniestros</li>
-            <li>-Estatus Gestión de Siniestros</li>
-        </SidebarCollapsible> -->
-
+    
     </PerfectScrollbar>
 
 
