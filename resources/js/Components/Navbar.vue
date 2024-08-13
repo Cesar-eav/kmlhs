4729b8ff3d08c29d108c3c8eca0c142d55c731a8
Cesar-eav <cesar.eav@hotmail.es>
2024-08-13 12:36:33 -0400

    Fix barra. Add seach sin funcionalidad

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,14 +1,11 @@
 <template>
-    <nav 
-        aria-label="secondary"
-        :class="[
-            'sticky top-0 z-10 px-4 py-2 sm:px-6 sm:py-4 bg-white flex items-center justify-between transition-transform duration-500 dark:bg-dark-eval-1',
-            {
-                '-translate-y-full': scrolling.down,
-                'translate-y-0': scrolling.up,
-            },
-        ]"
-    >
+    <nav aria-label="secondary" :class="[
+        'sticky inset-x-0 top-0 z-10 px-6 py-4 bg-white flex items-center justify-between transition-transform duration-500 dark:bg-dark-eval-1',
+        {
+            '-translate-y-full': scrolling.down,
+            'translate-y-0': scrolling.up,
+        },
+    ]">
         <div class="flex items-center gap-2">
             <!-- <Button
                 iconOnly
@@ -30,6 +27,13 @@
                     :class="iconSizeClasses"
                 />
             </Button> -->
+        </div>
+
+        <!-- Search Icon and Input -->
+        <div class="flex items-center gap-2">
+            <SearchIcon aria-hidden="true" class="w-10 h-5 text-gray-500" />
+            <input type="text" placeholder="Buscar..."
+                class="px-3 py-2 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-dark-eval-2 dark:text-gray-300" />
         </div>
         <div class="flex items-center gap-2">
             <!-- <Button
@@ -53,36 +57,25 @@
                 />
             </Button> -->
 
-            <Button
-                iconOnly
-                variant="secondary"
-                type="button"
-                @click="toggleFullScreen"
-                v-slot="{ iconSizeClasses }"
-                class="hidden md:inline-flex"
-                srText="Toggle dark mode"
-            >
-                <ArrowsExpandIcon
-                    v-show="!isFullscreen"
-                    aria-hidden="true"
-                    :class="iconSizeClasses"
-                />
-                <ArrowsInnerIcon
-                    v-show="isFullscreen"
-                    aria-hidden="true"
-                    :class="iconSizeClasses"
-                />
+            <Button iconOnly variant="secondary" type="button" @click="toggleFullScreen" v-slot="{ iconSizeClasses }"
+                class="hidden md:inline-flex" srText="Toggle dark mode">
+                <ArrowsExpandIcon v-show="!isFullscreen" aria-hidden="true" :class="iconSizeClasses" />
+                <ArrowsInnerIcon v-show="isFullscreen" aria-hidden="true" :class="iconSizeClasses" />
             </Button>
 
             <div class="relative" v-if="$page.props.jetstream.hasTeamFeatures">
                 <Dropdown align="right" width="60" v-if="$page.props.jetstream.hasTeamFeatures">
                     <template #trigger>
                         <span class="inline-flex rounded-md">
-                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white transition hover:bg-gray-50 hover:text-gray-700 dark:bg-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-300 dark:hover:bg-dark-eval-2 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
+                            <button type="button"
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white transition hover:bg-gray-50 hover:text-gray-700 dark:bg-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-300 dark:hover:bg-dark-eval-2 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
                                 {{ $page.props.user.current_team.name }}
 
-                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </span>
@@ -116,7 +109,12 @@
                                     <form @submit.prevent="switchToTeam(team)">
                                         <DropdownLink tag="button" type="submit">
                                             <div class="flex items-center">
-                                                <svg v-if="team.id == $page.props.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                <svg v-if="team.id == $page.props.user.current_team_id"
+                                                    class="mr-2 h-5 w-5 text-green-400" fill="none"
+                                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    stroke="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                                </svg>
                                                 <div>{{ team.name }}</div>
                                             </div>
                                         </DropdownLink>
@@ -131,16 +129,22 @@
             <div class="relative">
                 <Dropdown align="right" width="48">
                     <template #trigger>
-                        <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-md transition focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
-                            <img class="h-8 w-8 rounded-md object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name" />
+                        <button v-if="$page.props.jetstream.managesProfilePhotos"
+                            class="flex text-sm border-2 border-transparent rounded-md transition focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
+                            <img class="h-8 w-8 rounded-md object-cover" :src="$page.props.user.profile_photo_url"
+                                :alt="$page.props.user.name" />
                         </button>
 
                         <span v-else class="inline-flex rounded-md">
-                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white transition hover:bg-gray-50 hover:text-gray-700 dark:bg-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-300 dark:hover:bg-dark-eval-2 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
+                            <button type="button"
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white transition hover:bg-gray-50 hover:text-gray-700 dark:bg-dark-eval-1 dark:text-gray-400 dark:hover:text-gray-300 dark:hover:bg-dark-eval-2 focus:outline-none focus:ring focus:ring-purple-500 focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark-eval-1">
                                 {{ $page.props.user.name }}
 
-                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </span>
@@ -172,52 +176,30 @@
                 </Dropdown>
             </div>
         </div>
-    </nav> 
+    </nav>
 
     <!-- Mobile bottom bar -->
-    <div
-        :class="[
-            'fixed inset-x-0 bottom-0 flex items-center justify-between px-4 py-2 sm:px-6 sm:py-4 transition-transform duration-500 bg-white md:hidden dark:bg-dark-eval-1',
-            {
-                'translate-y-full': scrolling.down,
-                'translate-y-0': scrolling.up,
-            },
-        ]"
-    >
-        <Button
-            iconOnly
-            variant="secondary"
-            type="button"
-            v-slot="{ iconSizeClasses }"
-            srText="Search"
-        >
+    <div :class="[
+        'fixed inset-x-0 bottom-0 flex items-center justify-between px-4 py-2 sm:px-6 sm:py-4 transition-transform duration-500 bg-white md:hidden dark:bg-dark-eval-1',
+        {
+            'translate-y-full': scrolling.down,
+            'translate-y-0': scrolling.up,
+        },
+    ]">
+        <Button iconOnly variant="secondary" type="button" v-slot="{ iconSizeClasses }" srText="Search">
             <SearchIcon aria-hidden="true" :class="iconSizeClasses" />
         </Button>
 
         <Link :href="route('dashboard')">
-            <!-- <ApplicationLogo class="w-10 h-10" /> -->
-            <p class="w-10 h-10" >KML</p>
-            <span class="sr-only">KML</span>
+        <!-- <ApplicationLogo class="w-10 h-10" /> -->
+        <p class="w-10 h-10">KML S</p>
+        <span class="sr-only">KML</span>
         </Link>
 
-        <Button
-            iconOnly
-            variant="secondary"
-            type="button"
-            @click="sidebarState.isOpen = !sidebarState.isOpen"
-            v-slot="{ iconSizeClasses }"
-            srText="Toggle sidebar"
-        >
-            <MenuIcon
-                v-show="!sidebarState.isOpen"
-                aria-hidden="true"
-                :class="iconSizeClasses"
-            />
-            <XIcon
-                v-show="sidebarState.isOpen"
-                aria-hidden="true"
-                :class="iconSizeClasses"
-            />
+        <Button iconOnly variant="secondary" type="button" @click="sidebarState.isOpen = !sidebarState.isOpen"
+            v-slot="{ iconSizeClasses }" srText="Toggle sidebar">
+            <MenuIcon v-show="!sidebarState.isOpen" aria-hidden="true" :class="iconSizeClasses" />
+            <XIcon v-show="sidebarState.isOpen" aria-hidden="true" :class="iconSizeClasses" />
         </Button>
     </div>
 </template>
